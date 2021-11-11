f90e12223949388c197b8ab85007a97aa7fe991f
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-11 18:47:08 -0300

    add dynamic port api

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
+const app = express();
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
