d44c1161a03e6a6c986b2435e91c1d2f50332646
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-11 19:57:47 -0300

    update envs

--- src/Configs/db.js
+++ src/Configs/db.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  username: process.env.USERNAME || 'postgres',
+  username: process.env.DB_USERNAME || 'postgres',
   password: process.env.PASSWORD || 'postgres',
   database: process.env.DATABASE || 'postgres',
   port: process.env.PORT || 'postgres',
