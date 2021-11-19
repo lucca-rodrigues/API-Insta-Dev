63f77a86b427ee7d38ac04b85b8910dbe91fdac0
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-19 13:36:21 -0300

    add column table posts

---
+++ src/App/Database/Migrations/20211119162548-create-posts.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      author_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      number_likes: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("posts");
+  },
+};
