e8c63083b467f1d3e7684f85c41f342a2d6ec7aa
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-02 14:29:44 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -7,6 +8,7 @@ require("./App/Database");
 
 const routes = require("./routes");
 
+app.use(cors);
 app.use(express.json());
 
 app.use(routes);
