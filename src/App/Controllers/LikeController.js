bd33a0af3abbc55ac9a19ef1b8a755ba52935303
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-03 13:58:01 -0300

    create unlike post rules controller

--- src/App/Controllers/LikeController.js
+++ src/App/Controllers/LikeController.js
@@ -29,7 +29,11 @@ class LikeController {
 
     try {
       const addLikeOnPost = await Like.update(
-        { users_liked: [...listUsersLiked, userLiked], likes: likes.likes + 1 },
+        {
+          users_liked: [...listUsersLiked, userLiked],
+          liked: true,
+          likes: likes.likes + 1,
+        },
         { where: { post_id: postId } }
       );
 
@@ -40,71 +44,48 @@ class LikeController {
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
-
-    // return res.status(200).json({
-    //   message: "Success, post liked",
-    //   post_id: postId,
-    //   users_liked: [userLiked],
-    //   post: post,
-    // });
-
-    // try {
-    //   // const like = await Like.findAll();
-    //   const like = await Like.create({
-    //     post_id: postId,
-    //     liked: true,
-    //     users_liked: [...users_liked, req.user_id],
-    //     likes,
-    //   });
-    //   return res.status(200).json(like);
-    // } catch (err) {
-    //   return res.status(400).json({ error: err.message });
-    // }
-
-    // const like = await Like.create({
-    //   post_id,
-    //   liked,
-    //   users_liked,
-    //   likes,
-    // });
-
-    // if (!like) {
-    //   return res.status(400).json(like);
-    // }
-
-    // return res
-    //   .status(200)
-    //   .json({ data: { post_id, liked, users_liked, likes } });
-
-    // return res.json({ message: "Success, like added" });
   }
   async removeLike(req, res) {
     const postId = req.params.id;
+    const userLiked = req.user_id;
 
-    const verifyPost = await Post.findOne({
+    const post = await Post.findOne({
       where: { id: postId },
     });
 
-    if (!verifyPost) {
+    const likes = await Like.findOne({
+      where: { post_id: postId },
+    });
+
+    const listUsersLiked = likes.users_liked;
+    const thisUserAlreadyLike = likes.users_liked.includes(userLiked);
+    const removeUserLiked = listUsersLiked.filter((user) => user !== userLiked);
+
+    console.log("listUsersLiked", listUsersLiked);
+
+    if (!post) {
       return res.status(400).json({ error: "Post not found" });
     }
 
-    if (verifyPost.likes > 0) {
-      const postUnlike = await Post.update(
-        { likes: verifyPost.likes - 1 },
-        {
-          where: { id: postId },
-        }
-      );
+    if (thisUserAlreadyLike) {
+      try {
+        const addLikeOnPost = await Like.update(
+          {
+            users_liked: removeUserLiked,
+            likes: likes.likes - 1,
+          },
+          { where: { post_id: postId } }
+        );
 
-      if (!postUnlike) {
-        return res.status(400).json({ error: "Error to add like" });
+        return res.json({
+          message: "Success, post unliked",
+          status: addLikeOnPost,
+        });
+      } catch (err) {
+        return res.status(400).json({ error: err.message });
       }
-
-      return res.json({ message: "Success, post unliked" });
     }
-
-    return res.status(401).json({ message: "Post not have a like" });
+    return res.status(401).json({ error: "You didn't like this post" });
   }
 }
 
