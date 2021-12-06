d78b0628d919a63ca2762d0293f4197207ce0f4d
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-06 15:34:00 -0300

    refactory user bio routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const usersRoute = require("./users.routes");
+const usersBio = require("./userDetails.routes");
 
 const routes = Router();
 
@@ -7,5 +8,6 @@ routes.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
 routes.use("/users", usersRoute);
+routes.use("/users", usersBio);
 
 module.exports = routes;
---
+++ src/Routes/userDetails.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const authMiddleware = require("../App/Middlewares/auth");
+const schemaValidator = require("../App/Middlewares/schemaValidator");
+
+const { UserDetailsController } = require("../App/Controllers");
+// const { userSchema } = require("../Schemas");
+
+const routes = Router();
+
+routes.get("/bio", authMiddleware, UserDetailsController.getUserBioDetails);
+routes.post("/bio", authMiddleware, UserDetailsController.create);
+routes.put("/bio/:id", authMiddleware, UserDetailsController.update);
+routes.delete("/bio/:id", authMiddleware, UserDetailsController.delete);
+
+module.exports = routes;
